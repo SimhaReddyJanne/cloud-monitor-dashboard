0e437243d939a205bf6d5bf7de40a52cfc1e1d93
Simha Reddy Janne <simhareddy19@gmail.com>
2025-04-29 22:12:27 -0400

    Fix: added region_name to boto3 clients and host config for Render

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ def get_s3_buckets():
     s3 = boto3.client('s3', region_name='us-east-1')
     response = s3.list_buckets()
     buckets = []
+
     for bucket in response['Buckets']:
         buckets.append({
             'name': bucket['Name'],
             'creation_date': bucket['CreationDate']
         })
+
     return buckets
 
 # --- EC2 Monitoring ---
@@ -35,11 +37,13 @@ def get_iam_users():
     iam = boto3.client('iam', region_name='us-east-1')
     response = iam.list_users()
     users = []
+
     for user in response['Users']:
         users.append({
             'username': user['UserName'],
             'created_at': user['CreateDate']
         })
+
     return users
 
 # --- Route ---
@@ -48,13 +52,11 @@ def dashboard():
     ec2_instances = get_ec2_instances()
     s3_buckets = get_s3_buckets()
     iam_users = get_iam_users()
-    return render_template(
-        'dashboard.html',
-        ec2_instances=ec2_instances,
-        s3_buckets=s3_buckets,
-        iam_users=iam_users
-    )
+    return render_template('dashboard.html',
+                           ec2_instances=ec2_instances,
+                           s3_buckets=s3_buckets,
+                           iam_users=iam_users)
 
 # --- Run App ---
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
